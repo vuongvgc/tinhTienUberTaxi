5d361d260afe659871c79bbd785ded4c668a0f15
Phuong <ledinhphuong94@gmail.com>
2020-11-12 15:53:30 +0700

    Phuong update

--- js/Validation.js
+++ js/Validation.js
@@ -1,21 +1,27 @@
 /*
 *Đối Tượng: Validation kiểm tra thông tin form
 */
-function  ValidationCheckForm(){
+function ValidationCheckForm(){
     /*
     * Check không được để rổng
     * input: {dữ liệu từ input id} value 
     * input: {id thông báo} spanID  
     * input: {tin nhắn thông báo} message  
     * output: Boolean
-    */
-    
-    
-    
-    
-    
-    
-    
+    */  
+    this.kiemTraRong = function(value,spanID,message){
+        var isValid = true;
+        if(value == ""){
+            getEle(spanID).style.display ="block";
+            getEle(spanID).innerHTML = message;
+            isValid = false;
+        }else{
+            getEle(spanID).style.display ="none";
+        }
+        return isValid;
+        
+    }
+
     
     /*
     * Check input phải là số
@@ -25,11 +31,23 @@ function  ValidationCheckForm(){
     * output: Boolean
     */
     
-    
-    
-    
-    
-    
+    this.kiemTraSo = function(value,spanID,message){
+        var isValid = false;       
+        var SoFormat = /^[0-9\-=\[\]\\|.\/]*$/;
+
+        if(value.match(SoFormat)){
+            getEle(spanID).style.display ="none";
+            isValid = true;
+            
+        }else{
+            getEle(spanID).style.display ="block";
+            getEle(spanID).innerHTML = message;
+            isValid = false;
+
+        }
+        return isValid;
+    }
+  
     
     /*
     * Check ô chọn loại xe: phải chọn loại xe
@@ -38,13 +56,29 @@ function  ValidationCheckForm(){
     * input: {tin nhắn thông báo} message  
     * output: Boolean
     */
+   this.kiemTraLoaiXe = function(nameInput,spanID,message){
+        var checkLoaiXe = false; 
+        var radios = document.getElementsByName(nameInput);
+        for(var i=0; i< radios.length; i++){
+            if(radios[i].checked){               
+                checkLoaiXe = true;
+                console.log("abc");
+            }
+        }
+
+        if(checkLoaiXe){
+            getEle(spanID).style.display ="none";
+            return true;
+        }else{
+            getEle(spanID).style.display ="block";
+            getEle(spanID).innerHTML = message;
+            return false;
+        }
+       
+   }    
     
-    
-    
-    
-   
-    
-    
-    
-    
+  
 }
+    
+    
+    
